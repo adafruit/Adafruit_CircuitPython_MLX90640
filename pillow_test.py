5c9cef91be1153ce5bda4e579c6a77173694b52e
Lady Ada <limor@ladyada.net>
2019-12-02 02:10:56 -0500

    try interpolating with pillow - need gradient work but much faster now - 32 Hz works at full speed

--- pillow_test.py
+++ pillow_test.py
@@ -9,24 +9,20 @@ import os
 import math
 import numpy as np
 import pygame
-from scipy.interpolate import griddata
 from colour import Color
+from PIL import Image
 
 import mlx90640
 
-PRINT_TEMPERATURES = False
-PRINT_ASCIIART = True
+INTERPOLATE = 10
 
 # Set i2c freq to 1MHz in /boot/config.txt
 i2c = busio.I2C(board.SCL, board.SDA)
 
-#low range of the sensor (this will be blue on the screen)
-MINTEMP = 22.
-#high range of the sensor (this will be red on the screen)
-MAXTEMP = 35.
-#how many color values we can have
-COLORDEPTH = 1024
-INTERPOLATION = 3
+#low range of the sensor (this will be black on the screen)
+MINTEMP = 20.
+#high range of the sensor (this will be white on the screen)
+MAXTEMP = 50.
 
 # set up display
 os.environ['SDL_FBDEV'] = "/dev/fb0"
@@ -35,22 +31,15 @@ pygame.init()
 screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
 print(pygame.display.Info())
 
-#sensor is an 32x24 grid, multiply by desired interpolation
-width = INTERPOLATION * 32
-height = INTERPOLATION * 24
-displayPixelWidth = pygame.display.Info().current_w//width
-displayPixelHeight = pygame.display.Info().current_h//height
-
-
-# pylint: disable=invalid-slice-index
-points = [(math.floor(ix % 32), math.floor(ix / 32)) for ix in range(0, 24*32)]
-grid_x, grid_y = np.mgrid[0:31:width*1j, 0:24:height*1j]
-# pylint: enable=invalid-slice-index
-
 #the list of colors we can choose from
-blue = Color("indigo")
-colors = list(blue.range_to(Color("red"), COLORDEPTH))
-
+falsecolors = (Color("black"), Color("indigo"), Color("green"),  Color("yellow"),
+               Color("red"), Color("purple"), Color("white"))
+#how many color values we can have
+COLORDEPTH = (len(falsecolors)-1) * 50
+colors = []
+for i in range(len(falsecolors)-1):
+    colors += list(falsecolors[i].range_to(falsecolors[i+1], 50))
+print(COLORDEPTH, len(colors))
 #create the array of colors
 colors = [(int(c.red * 255), int(c.green * 255), int(c.blue * 255)) for c in colors]
 
@@ -59,6 +48,8 @@ screen.fill((255, 0, 0))
 pygame.display.update()
 screen.fill((0, 0, 0))
 pygame.display.update()
+sensorout = pygame.Surface((32, 24))
+
 #some utility functions
 def constrain(val, min_val, max_val):
     return min(max_val, max(min_val, val))
@@ -84,18 +75,25 @@ while True:
     
     print("Read 2 frames in %0.2f s" % (time.monotonic()-stamp))
 
-    pixels = [map_value(p, MINTEMP, MAXTEMP, 0, COLORDEPTH - 1) for p in frame]
-    #perform interpolation
-    bicubic = griddata(points, pixels, (grid_x, grid_y), method='cubic')
-    #draw everything
-    for jy, col in enumerate(bicubic):
-        for ix, pixel in enumerate(col):
-            if np.isnan(pixel):
-                continue
-            pygame.draw.rect(screen,
-                             colors[constrain(int(pixel), 0, COLORDEPTH-1)],
-                             (displayPixelWidth * jy, displayPixelHeight * ix,
-                              displayPixelWidth, displayPixelHeight))
+    pixels = [0] * 768
+    for i, pixel in enumerate(frame):
+        coloridx = map_value(pixel, MINTEMP, MAXTEMP, 0, COLORDEPTH - 1)
+        coloridx = int(constrain(coloridx, 0, COLORDEPTH-1))
+        pixels[i] = colors[coloridx]
+
+    for h in range(24):
+        for w in range(32):
+            pixel = pixels[h*32 + w]
+            sensorout.set_at((w, h), pixel)
+
+    #pixelrgb = [colors[constrain(int(pixel), 0, COLORDEPTH-1)] for pixel in pixels]
+    img = Image.new('RGB', (32, 24))
+    img.putdata(pixels)
+    img = img.resize((32*INTERPOLATE, 24*INTERPOLATE), Image.BICUBIC) 
+    img_surface = pygame.image.fromstring(img.tobytes(), img.size, img.mode)
+    pygame.transform.scale(img_surface.convert(), screen.get_size(), screen)
     pygame.display.update()
-    print("Processed 2 frames in %0.2f s" % (time.monotonic()-stamp))
+    print("Completed 2 frames in %0.2f s (%d FPS)" %
+          (time.monotonic()-stamp, 1.0 / (time.monotonic()-stamp)))
+
 
