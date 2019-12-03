7b44b51d0e7c3239000bd963e1ed8660bf6b7728
Lady Ada <limor@ladyada.net>
2019-12-02 20:39:42 -0500

    fix simpletest

--- examples/mlx90640_simpletest.py
+++ examples/mlx90640_simpletest.py
@@ -1,24 +1,18 @@
 import time
 import board
 import busio
-import mlx90640
+import adafruit_mlx90640
 
 PRINT_TEMPERATURES = False
 PRINT_ASCIIART = True
 
 i2c = busio.I2C(board.SCL, board.SDA, frequency=800000)
-while not i2c.try_lock():
-    pass
-print("I2C addresses found:", [hex(device_address)
-                               for device_address in i2c.scan()])
-i2c.unlock()
-mlx = mlx90640.MLX90640(i2c)
+
+mlx = adafruit_mlx90640.MLX90640(i2c)
 print("MLX addr detected on I2C")
 print([hex(i) for i in mlx.serial_number])
 
-mlx.refresh_rate = mlx90640.RefreshRate.REFRESH_32_HZ
-print(mlx.refresh_rate)
-print("refresh rate: ", pow(2, (mlx.refresh_rate-1)), "Hz")
+mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ
 
 frame = [0] * 768
 while True:
